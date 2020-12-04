3dd69c1166c6d19d332a6a79713b8e325eeb7871
James Sapp <jameseisapp@gmail.com>
2020-12-04 21:31:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -112,10 +112,10 @@ def changing_alan
     return programmer_hash
 end
 
-def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
 
+# add "Assembly" to Dennis Ritchie's languages array
+# return the entire updated hash
+def adding_to_dennis
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -131,8 +131,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    
     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
     return programmer_hash
-
 end
