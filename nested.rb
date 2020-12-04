a7dae007401917f686dd51dbdf614c02a110d89d
James Sapp <jameseisapp@gmail.com>
2020-12-04 21:31:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,10 +89,10 @@ def adding_matz
     return programmer_hash
 end
 
-def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
 
+# change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+# return the entire updated hash
+def changing_alan
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -108,10 +108,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-    
     programmer_hash[:alan_kay][:known_for] = "GUI"
     return programmer_hash
-
 end
 
 def adding_to_dennis
